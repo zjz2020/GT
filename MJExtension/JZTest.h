2050c7af89c75cebc82604917718b9ad925c299b
zjz <zjz@zjzdeMacBook-Pro.local>
2020-04-02 15:17:12 +0800

    修改方法

---
+++ MJExtension/JZTest.h
@@ -0,0 +1,17 @@
+//
+//  JZTest.h
+//  GitTest
+//
+//  Created by zjz on 2020/4/2.
+//  Copyright © 2020 zjz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JZTest : NSObject
++ (void)ggTest;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MJExtension/JZTest.m
@@ -0,0 +1,15 @@
+//
+//  JZTest.m
+//  GitTest
+//
+//  Created by zjz on 2020/4/2.
+//  Copyright © 2020 zjz. All rights reserved.
+//
+
+#import "JZTest.h"
+
+@implementation JZTest
++ (void)ggTest{
+    NSLog(@"ggtest");
+}
+@end
