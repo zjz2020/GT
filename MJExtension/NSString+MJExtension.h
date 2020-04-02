924fe806e5cd8f4c04eda09e6d43957a2d250539
zjz <zjz@zjzdeMacBook-Pro.local>
2020-04-02 14:39:17 +0800

    修改方法

--- MJExtension/NSString+MJExtension.h
+++ MJExtension/NSString+MJExtension.h
@@ -7,27 +7,9 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "MJExtensionConst.h"
 
 @interface NSString (MJExtension)
-/**
- *  驼峰转下划线（loveYou -> love_you）
- */
-- (NSString *)mj_underlineFromCamel;
-/**
- *  下划线转驼峰（love_you -> loveYou）
- */
-- (NSString *)mj_camelFromUnderline;
-/**
- * 首字母变大写
- */
-- (NSString *)mj_firstCharUpper;
-/**
- * 首字母变小写
- */
-- (NSString *)mj_firstCharLower;
 
-- (BOOL)mj_isPureInt;
 
-- (NSURL *)mj_url;
+- (NSString *)mj_url;
 @end
